dd3c414acbcdfdd3d4bec95301e03cf41646ae5a
Eduardo <joselincango73@gmail.com>
2024-06-28 15:23:10 -0500

    Ajustes de tamaño del form

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // para cerrar el frame
         frame.setContentPane(new Plana().MainPanel);
         frame.setSize(800, 600);
-        frame.setPreferredSize(new Dimension(800, 600));
+        frame.setPreferredSize(new Dimension(550, 300));
         frame.pack();
         frame.setLocationRelativeTo(null);
 
